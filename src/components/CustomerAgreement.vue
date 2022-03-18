879ab7c0042ce974caec6a7a3516eae4b7806e1d
Fábio Paixão <fabiopaixao@live.com>
2022-03-18 05:41:25 -0300

    atualização 18-03-2022 05:41

--- src/components/CustomerAgreement.vue
+++ src/components/CustomerAgreement.vue
@@ -1,9 +1,29 @@
 <template>
-    <div>Agreement</div>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-card>
+                        <v-toolbar color="grey darken-4" dark>
+                            Orçamento
+                        </v-toolbar>
+                        <v-card-text>
+                            Agreement
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data: () => ({
+        //
+    }),
+    methods: {
+        //
+    }
 }
 </script>
