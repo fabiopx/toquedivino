cd82870b9125561e9114c4b224a0967946dd9327
Fábio Paixão <fabiopaixao@live.com>
2022-03-21 05:41:09 -0300

    atualização 21-03-2022 05:41

--- src/components/CustomerAgreement.vue
+++ src/components/CustomerAgreement.vue
@@ -7,27 +7,87 @@
             <v-toolbar color="grey darken-4" dark> Orçamento </v-toolbar>
             <v-card-text>
               <v-banner single-line>
-                <v-icon>mdi-account-music</v-icon>
-                Formação: {{ formation }}
+                <h3>Formação</h3>
+                <p class="font-italic">{{ formation.name }}</p>
+                <p>{{ formation.description }}</p>
+                <v-chip color="grey darken-4" dark
+                  >R$ {{ formation.price }}</v-chip
+                >
                 <template v-slot:actions>
-                  <v-btn depressed>Confirmar</v-btn>
-                  <v-btn depressed>Editar</v-btn>
+                  <v-btn icon @click="checkedFormation(), addFormation()">
+                    <v-icon v-show="!formationChecked"
+                      >mdi-checkbox-blank-circle-outline</v-icon
+                    >
+                    <v-icon v-show="formationChecked"
+                      >mdi-checkbox-marked-circle</v-icon
+                    >
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-circle-edit-outline</v-icon>
+                  </v-btn>
                 </template>
               </v-banner>
               <v-banner single-line>
-                <v-icon>mdi-music-circle</v-icon>
-                Serviço: {{ service }}
+                <h3>Serviço</h3>
+                <p class="font-italic">{{ service.name }}</p>
+                <p>{{ service.description }}</p>
+                <!-- <div v-show="tax">
+                  <h4>{{ tax.name }}</h4>
+                  <p>{{ tax.description }}</p>
+                  <v-chip>R$ {{ tax.value }} <span v-if="tax.type == '2'">{{ tax.multiplied }}</span></v-chip>
+                </div> -->
                 <template v-slot:actions>
-                  <v-btn depressed>Confirmar</v-btn>
-                  <v-btn depressed>Editar</v-btn>
+                  <v-btn icon @click="checkedService()">
+                    <v-icon v-show="!serviceChecked"
+                      >mdi-checkbox-blank-circle-outline</v-icon
+                    >
+                    <v-icon v-show="serviceChecked"
+                      >mdi-checkbox-marked-circle</v-icon
+                    >
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-circle-edit-outline</v-icon>
+                  </v-btn>
                 </template>
               </v-banner>
-              <v-banner two-line>
-                <v-icon>mdi-clock-time-three</v-icon>
-                Evento:
+              <v-banner single-line>
+                <h3>Evento</h3>
+                <p>Data: {{ events.date }}</p>
+                <p>Horário: {{ events.time }}</p>
+                <p>Endereço: 
+                  {{ events.address.street }}, 
+                  {{ events.address.number }} 
+                  {{ (events.address.complement) ? ", " + events.address.complement : "" }}
+                  {{ events.address.neigborhood }},
+                  {{ events.address.city }} -
+                  {{ events.address.state }},
+                  {{ events.address.zipcode }}
+                  </p>
+                  <template v-slot:actions>
+                  <v-btn icon @click="checkedEvent()">
+                    <v-icon v-show="!eventChecked"
+                      >mdi-checkbox-blank-circle-outline</v-icon
+                    >
+                    <v-icon v-show="eventChecked"
+                      >mdi-checkbox-marked-circle</v-icon
+                    >
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-circle-edit-outline</v-icon>
+                  </v-btn>
+                </template>
               </v-banner>
               <v-banner>
-                <b>Total:</b>
+                <b class="text-uppercase"
+                  >Total:
+                  {{
+                    budgetSoma.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })
+                  }}</b
+                >
+                
               </v-banner>
               <v-divider></v-divider>
               <v-btn color="grey darken-4" class="mt-3 pa-4" dark>
@@ -42,13 +102,81 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   data: () => ({
-    formation: "",
-    service: "",
+    formation: {},
+    service: {},
+    events:{},
+    formationChecked: false,
+    serviceChecked: false,
+    eventChecked: false,
+    budgetItems: [],
+    budgetSoma: 0,
+    distance: '',
+    tax:'',
   }),
   methods: {
-    //
+    ...mapActions(["setInscribeID", "setUserNow"]),
+    somaBudget: function (item) {
+      this.budgetSoma = parseFloat(this.budgetSoma) + parseFloat(item);
+    },
+    subtractBudget: function (item) {
+      this.budgetSoma = parseFloat(this.budgetSoma) - parseFloat(item);
+    },
+    addFormation: function () {
+      this.formationChecked
+        ? this.somaBudget(this.formation.price)
+        : this.subtractBudget(this.formation.price);
+    },
+    checkedFormation: function () {
+      this.formationChecked = !this.formationChecked;
+    },
+    checkedService: function () {
+      this.serviceChecked = !this.serviceChecked;
+    },
+    checkedEvent: function(){
+      this.eventChecked = !this.eventChecked;
+    },
+    getInscribe: async function () {
+      const response = await axios.get(
+        process.env.VUE_APP_URL + "getInscribeCustomers/" + this.userNow.id
+      );
+      this.formation = response.data.formation;
+      this.service = response.data.service;
+      this.getServiceTax();
+    },
+    getEvent: async function(){
+      const response = await axios.get(process.env.VUE_APP_URL + "getEventCustomers/" + this.inscribeID);
+      this.events = response.data;
+      this.getDistance();
+    },
+    getServiceTax: async function(){
+      const response = await axios.get(process.env.VUE_APP_URL + "getServices/" + this.service.idservice);
+      console.log(response.data.taxas)
+      this.tax = response.data.taxas
+    },
+    getDistance() {
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix({
+        origins: ['Americana-SP'],
+        destinations: [this.events.address.state],
+        travelMode: 'DRIVING',
+      }).then((response) => {
+        console.log(response.rows[0].elements[0].distance.value)
+      })
+    },
+  },
+  created: function () {
+    if (this.$session.exists()) {
+      this.setUserNow(this.$session.get("userData"));
+      this.getInscribe();
+      this.getEvent();
+    }
+  },
+  computed: {
+    ...mapGetters(["inscribeID", "userNow", "isAgreement"]),
   },
 };
 </script>
