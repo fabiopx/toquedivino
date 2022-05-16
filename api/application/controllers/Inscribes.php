dd3980353aaea6b54070c01377f238a88542d8d8
Fábio Paixão <fabiopaixao@live.com>
2022-05-16 15:00:08 -0300

    atualização 16-05-2022 15:00

--- api/application/controllers/Inscribes.php
+++ api/application/controllers/Inscribes.php
@@ -97,12 +97,12 @@ class Inscribes extends CI_Controller{
                         <table border="0" cellpadding="0" cellspacing="0" width="100%">
                         <tr>
                             <td>
-                               <img src="https://app.cerimonialtoquedivino.com.br/assets/img/logotipo.png" alt="logotipo" width="200" />
+                               <img src="https://api.cerimonialtoquedivino.com.br/assets/img/logotipo.png" alt="logotipo" width="200" />
                             </td>
                             <td>Olá '.$account->name.'</td>
                         </tr>
                         <tr>
-                            <td>Acesso: {unwrap}https://toquedivino.com/divine/customers{/unwrap}</td>
+                            <td>Acesso: {unwrap}https://app.cerimonialtoquedivino.com.br/customers{/unwrap}</td>
                         </tr>
                         <tr>
                             <td>Login: '.$account->email.'</td>
